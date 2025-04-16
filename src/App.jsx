54df8d8d4f20301340ea5aab98d620ce0979263b
FengJie <fllying8791@gmail.com>
2025-04-16 01:42:07 -0700

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import { useState, useEffect } from 'react';
 const App = () => {
   const [pokemon, setPokemon] = useState([]);
   const [flipStates, setFlipStates] = useState(Array(6).fill(false));
+  const [isShowAll, setIsShowAll] = useState(true);
+  const [isHidden, setIshidden] = useState(false);
+  const [selected, setSelected] = useState('official-artwork');
+  const options = ['dream_world', 'home', 'official-artwork', 'showdown'];
   const handleCardFlip = (index) => {
     setFlipStates((prev) => {
       const newFlips = [...prev];
@@ -12,13 +16,23 @@ const App = () => {
       return newFlips;
     });
   };
-
-  const handleClearPokemon = () => {
+  const handleChange = (e) => {
+    setSelected(e.target.value);
+    console.log('choice', e.target.value);
+  };
+  const handleRerollPokemon = () => {
+    setIshidden(true);
     fetchPokemons();
+    setTimeout(() => {
+      setIshidden(false);
+    }, 1000);
   };
 
   const handleShowAll = () => {
-    setFlipStates(Array(6).fill(true));
+    isShowAll
+      ? setFlipStates(Array(6).fill(true))
+      : setFlipStates(Array(6).fill(false));
+    setIsShowAll(!isShowAll);
   };
 
   const fetchPokemons = async () => {
@@ -26,9 +40,9 @@ const App = () => {
     for (let i = 1; i <= 6; i++) {
       try {
         const res = await fetch(
-          `https://pokeapi.co/api/v2/pokemon/${Math.floor(
-            Math.random() * 1000
-          )}`
+          `https://pokeapi.co/api/v2/pokemon/${
+            Math.floor(Math.random() * 151) + 1
+          }`
         );
         if (!res.ok) {
           throw new Error(`HTTP error status: ${res.status}`);
@@ -49,22 +63,40 @@ const App = () => {
 
   return (
     <div>
+      <header className='header-container'>
+        <h1 className='h1-text'>Create Your Pokemon Team, Randomly</h1>
+      </header>
       <div className='cards-container'>
         {pokemon.map((poke, index) => (
           <Card
+            isHidden={isHidden}
             key={index}
             id={poke.id}
             name={poke.name}
-            img={poke.sprites}
+            img={poke.sprites.other[selected].front_default}
             isFlipped={flipStates[index]}
             onFlip={() => handleCardFlip(index)}
+            types={poke.types}
           />
         ))}
       </div>
-      .
+
+      <div className='dropdown-menu'>
+        <label htmlFor='image-source'>Choose the Image Art Work </label>
+        <select id='image-source' value={selected} onChange={handleChange}>
+          {options.map((opt) => (
+            <option key={opt} value={opt}>
+              {opt}
+            </option>
+          ))}
+        </select>
+      </div>
       <div className='btn-container'>
-        <Button handleOnClick={handleClearPokemon} name={'Roll Again'} />
-        <Button handleOnClick={handleShowAll} name={'Show All'} />
+        <Button handleOnClick={handleRerollPokemon} name={'Roll Again'} />
+        <Button
+          handleOnClick={handleShowAll}
+          name={isShowAll ? 'Show All' : 'Hide All'}
+        />
       </div>
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,59 @@
 import React from 'react';
-
-const Card = ({ id, name, img, onFlip, isFlipped }) => {
+const type = {
+  normal: 1,
+  fighting: 2,
+  flying: 3,
+  poison: 4,
+  ground: 5,
+  rock: 6,
+  bug: 7,
+  ghost: 8,
+  steel: 9,
+  fire: 10,
+  water: 11,
+  grass: 12,
+  electric: 13,
+  psychic: 14,
+  ice: 15,
+  dragon: 16,
+  dark: 17,
+  fairy: 18,
+};
+const Card = ({ id, name, img, onFlip, isFlipped, isHidden, types }) => {
+  // const randomId = Math.floor(Math.random() * 4) + 1;
   const nameWithUpperCase = name.charAt(0).toUpperCase() + name.slice(1);
-
+  console.log(types);
   return (
     <div
-      className={`flip-container ${isFlipped ? '' : 'flip'}`}
+      className={`flip-container ${isFlipped ? '' : 'flip'} ${
+        isHidden ? 'hide' : ''
+      }`}
       onClick={onFlip}
     >
       <div className='card-flipper'>
-        <div className='card card-front'>
+        <div
+          className='card card-front'
+          // style={{
+          //   backgroundImage: `url('/src/assets/images/card-bg-1.png')`,
+          // }}
+        >
           <div className='title'>
-            {id}
-            <span className='font-600'> {nameWithUpperCase}</span>
+            <span className='card-font-name'> {nameWithUpperCase}</span>
+            <span className='card-font-id'>{id}</span>
           </div>
           <div className='img-box'>
-            <img
-              className='img-big'
-              src={img.other['official-artwork'].front_default}
-              alt=''
-            />
+            <img className='img-big' src={img} alt='' />
+          </div>
+          <div className='types'>
+            {types.map((el, index) => (
+              <img
+                key={index}
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-viii/sword-shield/${
+                  type[el.type.name]
+                }.png`}
+                alt=''
+              />
+            ))}
           </div>
         </div>
         <div className='card card-back'></div>
